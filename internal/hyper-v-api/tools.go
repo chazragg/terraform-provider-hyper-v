4f1ec25b99a36ab4cd664258349d69c4d4754593
Charles Ragg <chazragg@gmail.com>
2025-03-01 22:36:41 +0000

    refactor 🎨(tools): Improved template rendering logging
    Signed-off-by: Charles Ragg <chazragg@gmail.com>

--- internal/hyper-v-api/tools.go
+++ internal/hyper-v-api/tools.go
@@ -13,24 +13,28 @@ import (
 func renderTemplate(templateFile string, data any) (*bytes.Buffer, error) {
 	tmpl, err := template.ParseFS(templatesFS, "templates/"+templateFile)
 	if err != nil {
-		log.Fatalf("Error Parsing FS: %v", err)
-		return nil, fmt.Errorf("failed to parse template: %w", err)
+		return nil, fmt.Errorf("failed to parse template %s: %w", templateFile, err)
 	}
 
 	var script bytes.Buffer
 	if err := tmpl.Execute(&script, data); err != nil {
-		log.Fatalf("Error Executing Template: %v", err)
-		return nil, fmt.Errorf("failed to execute template: %w", err)
+		return nil, fmt.Errorf("failed to execute template %s: %w", templateFile, err)
 	}
 
 	return &script, nil
 }
 
 func runRemoteCommand(ctx context.Context, client *winrm.Client, script string) (string, string, int, error) {
+	// TODO: Move error logging to this level for all scripts
 	log.Println("Running Remote command")
 	stdout, stderr, exitCode, err := client.RunPSWithContext(ctx, script)
+	log.Println(script)
 	if err != nil {
 		return "", "", 0, fmt.Errorf("failed to run remote command: %w", err)
+	}
+
+	if exitCode != 0 {
+		return "", stderr, exitCode, nil
 	}
 
 	return stdout, stderr, exitCode, nil
